3ea2355be7d3ccdd0af283c439856bf83bccdeae
NeetuKV <pranee6r@yahoo.com>
2023-01-25 14:36:46 -0800

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <button className="btn btn-primary">Button</button>
-      <footer>
-        Coded by Praneetha.
-        <a href="https://github.com/NeetuKV/react-forecast-app">
-          Open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <button className="btn btn-primary">Button</button>
+        <footer>
+          Coded by Praneetha.
+          <a href="https://github.com/NeetuKV/react-forecast-app">
+            Open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
