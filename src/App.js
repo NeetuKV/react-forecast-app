c55188ad5924241cc93719908c71237f1633e23e
NeetuKV <pranee6r@yahoo.com>
2023-01-25 09:39:06 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather Application</h1>
+      <button className="btn btn-primary">Button</button>
       <footer>
         Coded by Praneetha.
         <a href="https://github.com/NeetuKV/react-forecast-app">
