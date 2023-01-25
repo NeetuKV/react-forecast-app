ed1094b1ee9509e9bcf4791e3d0049d2ce94b50d
NeetuKV <pranee6r@yahoo.com>
2023-01-25 09:14:46 -0800

    added a link to github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application :)</div>;
+  return (
+    <div className="App">
+      <h1>Weather Application :)</h1>
+      <footer>
+        Coded by Praneetha.
+        <a href="https://github.com/NeetuKV/react-forecast-app" target="_blank">
+          Open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
