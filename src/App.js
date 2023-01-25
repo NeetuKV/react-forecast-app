27bacbb36cc4b499c70e65eac5d1be3597b08078
NeetuKV <pranee6r@yahoo.com>
2023-01-25 09:22:32 -0800

    removed target blank

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Application :)</h1>
+      <h1>Weather Application</h1>
       <footer>
         Coded by Praneetha.
-        <a href="https://github.com/NeetuKV/react-forecast-app" target="_blank">
+        <a href="https://github.com/NeetuKV/react-forecast-app">
           Open sourced on GitHub
         </a>
       </footer>
