78525047f50130703c9c3437054f72cdcb8158f1
NeetuKV <pranee6r@yahoo.com>
2023-01-31 09:47:06 -0800

    changed forwcast map

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -9,15 +8,21 @@ export default function WeatherForecast(props) {
   let [forecast, setForecast] = useState(null);
   function handleResponse(response) {
     setForecast(response.data.daily);
-    setForecast(true);
+    setLoaded(true);
   }
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
