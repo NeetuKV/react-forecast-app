83fad135dfa8a99fb7f527f84171d195711edcd5
NeetuKV <pranee6r@yahoo.com>
2023-02-01 09:14:09 -0800

    added useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
@@ -30,7 +34,7 @@ export default function WeatherForecast(props) {
     let apiKey = "ff1d9ea9376b5c27a82e04fc2b2abdbb";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiURL).then(handleResponse);
     return null;
   }
